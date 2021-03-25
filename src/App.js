41f44f29a1b781a584edd6038ae6acf7e434cc1d
Reinoptland <reinoptland@gmail.com>
2021-03-25 10:06:00 +0100

    Return function from useEffect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [counter, setCounter] = useState(0);
   const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true);
 
   console.log("WHAT IS COUNT:", counter); // 0 -> 1 -> 2
 
@@ -13,13 +14,23 @@ function App() {
     async function fetchQuote() {
       const response = await axios.get("https://api.kanye.rest/");
       setQuote(response.data.quote);
+      setLoading(false);
     }
 
     fetchQuote();
-  }, []); // wanneer moet dit effect opnieuw worden uitgevoerd?
+
+    return () => {
+      // clean up
+      setLoading(true);
+      setQuote("");
+      console.log("I GET RUN BEFORE THE NEXT EFFECT TAKES PLACE");
+    };
+  }, [counter]); // "" => "Tesla is cool" => "Stare in the mirror"
+  // wanneer moet dit effect opnieuw worden uitgevoerd?
 
   return (
     <div className="App">
+      {loading && <h1>LOADING</h1>}
       <p>{quote}</p>
       <h1>{counter}</h1>
       <button onClick={() => setCounter(counter + 1)}>+</button>
